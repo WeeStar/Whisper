a4f55848515a4bbd7dc9cdd0e9ee0004124f9420
WeeStar <402284187>
2020-06-10 16:01:38 +0800

    init

---
+++ Whisper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Whisper
+//
+//  Created by WeeStar on 2020/6/10.
+//  Copyright © 2020 WeeStar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
